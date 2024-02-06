639635cf41744370420d273c0d2e6939dd627c87
Enrico Comiti <enrico.comiti@gmail.com>
2024-02-06 21:03:05 +0100

    Initial commit

---
+++ src/main/java/com/github/enr/idealwinner/LibraryService.java
@@ -0,0 +1,17 @@
+package com.github.enr.idealwinner;
+
+/**
+ * Library service does this...
+ */
+public class LibraryService {
+
+  /**
+   * Operate does this...
+   *
+   * @param input
+   * @return the input!
+   */
+  public String operate(String input) {
+    return "%s!".formatted(input);
+  }
+}
